416341c4d99bb3e1d53e6c2a8e458777192f1586
Yann de The <ydethe@gmail.com>
2020-10-01 09:13:05 +0000

    Code mis plus au propre

--- CollegesLycees/import_lycees.py
+++ CollegesLycees/import_lycees.py
@@ -7,22 +7,11 @@ import tqdm
 from init_db import Lycee, corr_acces, corr_reussite, corr_mention
 
 
-# https://www.education.gouv.fr/les-indicateurs-de-resultats-des-lycees-1118
-# https://www.data.gouv.fr/fr/datasets/diplome-national-du-brevet-par-etablissement
-
-
-engine = create_engine('sqlite:///lycee.db')
-session = sessionmaker()
-session.configure(bind=engine)
-s = session()
-
-# df_c = pd.read_excel (open('menesr-depp-dnb-session-2018.xls', 'rb'), sheet_name='Sheet')
-df = pd.read_excel('ival-2018-donn-es--32258.xls', sheet_name=None)
-
-def import_sheet(name, corr_dict):
-    print("Importation %s..." % name)
-    n = len(df[name].index)
-    for index, row in tqdm.tqdm(df[name].iterrows(), total=n):
+def import_sheet(session, file, sheet_name, corr_dict):
+    df = pd.read_excel('ival-2018-donn-es--32258.xls', sheet_name=sheet_name)
+    print("Importation %s..." % sheet_name)
+    n = len(df.index)
+    for index, row in tqdm.tqdm(df.iterrows(), total=n):
         dat = {}
         for xl_k in corr_dict.keys():
             db_k,fct = corr_dict[xl_k]
@@ -30,17 +19,27 @@ def import_sheet(name, corr_dict):
             if not val is None:
                 dat[db_k] = val
 
-        istat = s.query(Lycee).\
+        istat = session.query(Lycee).\
            filter(Lycee.UAI==dat['UAI']).\
            update(dat)
         if istat == 0:
             enr = Lycee(**dat)
-            s.add(enr)
+            session.add(enr)
 
-    s.commit()
+    session.commit()
 
-import_sheet('ACCES_GT', corr_acces)
-import_sheet('REUSSITE_GT', corr_reussite)
-import_sheet('MENTIONS_GT', corr_mention)
+
+# https://www.education.gouv.fr/les-indicateurs-de-resultats-des-lycees-1118
+# https://www.data.gouv.fr/fr/datasets/diplome-national-du-brevet-par-etablissement
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///lycee.db')
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+
+    import_sheet(s, 'ival-2018-donn-es--32258.xls', 'ACCES_GT', corr_acces)
+    import_sheet(s, 'ival-2018-donn-es--32258.xls', 'REUSSITE_GT', corr_reussite)
+    import_sheet(s, 'ival-2018-donn-es--32258.xls', 'MENTIONS_GT', corr_mention)
 
 
