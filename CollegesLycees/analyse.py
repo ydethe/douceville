2f05c7eed788b6d16b483eec20ad9cf4a3f4546d
Yann de The <ydethe@gmail.com>
2020-10-01 14:15:15 +0000

    Petite analyse

--- CollegesLycees/analyse.py
+++ CollegesLycees/analyse.py
@@ -1,18 +1,19 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from CollegesLycees.init_db import Lycee
+from init_db import Lycee
 
 
-engine = create_engine('sqlite:///lycee.db')
+engine = create_engine('sqlite:///lycee_gt_pro.db')
 session = sessionmaker()
 session.configure(bind=engine)
 s = session()
 
 # result = s.query(Lycee).all()
-result = s.query(Lycee).filter(Lycee.departement == 31).filter(Lycee.secteur_prive).all()
+# result = s.query(Lycee).filter(Lycee.departement == 31).filter(Lycee.secteur_prive).all()
+result = s.query(Lycee).filter(Lycee.departement == 31).filter(Lycee.latitude.is_(None)).all()
 
 print(len(result))
-# for row in result:
-#     print(row.UAI)
+for row in result:
+    print(row.nom)
 
