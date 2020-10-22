ca1625a1cde5246f719f8d678122567b9164d720
Yann de The <ydethe@gmail.com>
2020-10-23 00:50:30 +0200

    Production OK sur johncloud.fr

--- maillage/config.py
+++ maillage/config.py
@@ -6,11 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://ydethe:m41ll4g3@ydethe.mysql.pythonanywhere-services.com/ydethe$maillage"
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://cl_user@localhost/maillage"
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "etablissements.db")
+    # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://cl_user@localhost/maillage"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "etablissements.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    HOST = "localhost"
-    PORT = 8000
+    HOST = "https://johncloud.fr"
+    PORT = 8086
     # HOST = "https://johncloud.fr"
     # PORT = 8000
     SECRET_KEY = "ITSASECRET"
