99d87f1cdf5fd191b8ec770b35843bafdd23163e
Yann de The <ydethe@gmail.com>
2020-10-28 13:13:28 +0000

    Test join

---
+++ test_join.py
@@ -0,0 +1,5 @@
+﻿from douceville.models import *
+
+
+for e,r in db.session.query(Etablissement,Resultat).filter(Etablissement.UAI=='0290064M').filter(Etablissement.UAI==Resultat.etablissement_id).all():
+    print(r,r.annee)
