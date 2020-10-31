5ac63c37fbe1f4241f17ad52e14ebc5f5f82c7b5
Yann de Thé <ydethe@gmail.com>
2020-10-31 22:07:53 +0100

    Bootstrap OK

--- douceville/routes.py
+++ douceville/routes.py
@@ -2,15 +2,13 @@ from flask import render_template
 from sqlalchemy import distinct
 
 from douceville import app
-from douceville.models import db, Etablissement
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     user = {"username": "Yann"}
-    r = db.session.query(distinct(Etablissement.nature)).all()
 
     return render_template(
-        "index.html", title="Home", user=user, natures=[x[0] for x in r]
+        "index.html", title="Home", user=user
     )
