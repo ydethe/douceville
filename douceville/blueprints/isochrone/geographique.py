a26b21898cce557f320672bad786c51b1dac7e39
Yann de Thé <ydethe@gmail.com>
2020-11-06 22:45:09 +0100

    Correction geographique pour blueprint carte

--- douceville/blueprints/isochrone/geographique.py
+++ douceville/blueprints/isochrone/geographique.py
@@ -24,6 +24,23 @@ def calcIsochrone(center, dist):
 
     return iso
 
+@logged
+def geocodeUserAddress(query, logger=None):
+    api_key = Config.OPENROUTESERVICE_KEY
+    clnt = client.Client(key=api_key)
+
+    lon=lat=None
+    j = geocode.pelias_search(
+        clnt,
+        query,
+        country="FR",
+        layers=["postalcode", "address", "locality", "venue"],
+    )
+    for f in j["features"]:
+        lon, lat = f["geometry"]["coordinates"]
+
+        return lon,lat
+
 
 @logged
 def findCoordFromAddress(nom=None, adresse=None, cp=None, commune=None, lat=None, lon=None, logger=None):
