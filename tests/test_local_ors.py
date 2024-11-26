ee205301e8f85ae39bd39330bb3d5569000eec2d
Yann de Thé <ydethe@gmail.com>
2024-11-26 14:00:20 +0100

    Updated local ors test

--- tests/test_local_ors.py
+++ tests/test_local_ors.py
@@ -2,20 +2,21 @@ from openrouteservice import Client
 
 
 def test_ors():
-    coords = ((8.34234, 48.23424), (8.34423, 48.26424))
+    coords = ((1.5365123, 43.544737), (1.3456123,43.615147))
 
     # key can be omitted for local host
-    client = Client(base_url="http://localhost:8082/ors")
+    # client = Client(base_url="https://ors.toulouse.ecoledessarments.com/ors")
+    client = Client(base_url="localhost:8082/ors")
 
     # Only works if you didn't change the ORS endpoints manually
     routes = client.directions(coords)
 
     # If you did change the ORS endpoints for some reason
     # you'll have to pass url and required parameters explicitly:
-    routes = client.request(
-        url="/new_url",
-        post_json={"coordinates": coords, "profile": "driving-car", "format": "geojson"},
-    )
+    # routes = client.request(
+    #     url="/new_url",
+    #     post_json={"coordinates": coords, "profile": "driving-car", "format": "geojson"},
+    # )
 
     print(routes)
 
