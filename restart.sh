f70ae93f7f7a015abe7de017b68bc030b186fcf4
Yann de The <ydethe@gmail.com>
2024-11-26 09:13:50 +0100

    wip

--- restart.sh
+++ restart.sh
@@ -4,5 +4,6 @@ rm -f *.whl *.log
 pdm build
 cp dist/*.whl .
 pdm export --prod -o requirements.txt
+sudo docker compose -f docker-compose.dev.yml stop
 sudo docker compose -f docker-compose.dev.yml up --build --remove-orphans -d
 sudo docker compose -f docker-compose.dev.yml logs -f
