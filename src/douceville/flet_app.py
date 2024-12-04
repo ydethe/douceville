d1683abd8f4970d63d13640a01b7582e2827ac35
Yann de The <ydethe@gmail.com>
2024-12-04 14:57:35 +0100

    fle p roto with hypercorn OK

---
+++ src/douceville/flet_app.py
@@ -0,0 +1,27 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    counter = ft.Text("0", size=50, data=0)
+
+    def increment_click(e):
+        counter.data += 1
+        counter.value = str(counter.data)
+        counter.update()
+
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.ADD, on_click=increment_click
+    )
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                counter,
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+
+
+# hypercorn douceville.flet_app:app --bind 0.0.0.0:3566
+app = ft.app(main, export_asgi_app=True)
