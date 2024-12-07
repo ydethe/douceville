e3fdd08aec5e92522dc23e699c26a73e620c12de
Yann de The <ydethe@gmail.com>
2024-12-07 20:13:38 +0100

    Tests OK

--- src/douceville/rest_api_entreypoint.py
+++ src/douceville/rest_api_entreypoint.py
@@ -98,7 +98,7 @@ def read_etablissement(
     db: Session = Depends(get_db),
 ) -> DvUser:
     etab = get_etab(db, uai)
-
+    # TODO: Handle the case where no etablissement is found
     return etab
 
 
