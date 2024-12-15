188ab3a4c346aef8b99b810547379f6a0fdcc130
Yann de The <ydethe@gmail.com>
2024-12-15 08:58:41 +0100

    Fixed supabasefreeze

--- tests/test_supabase.py
+++ tests/test_supabase.py
@@ -4,24 +4,13 @@ from douceville.config import config
 from douceville.schemas import DvUser
 
 
-def test_supabase() -> str:
+def test_supabase():
     supabase: Client = create_client(config.SUPABASE_URL, config.SUPABASE_KEY)
     response = supabase.auth.sign_in_with_password(
         {"email": config.SUPABASE_TEST_USER, "password": config.SUPABASE_TEST_PASSWORD}
     )
 
     token = response.session.access_token
-
-    return token
-
-
-def test_supabase2(
-    token: str = "eyJhbGciOiJIUzI1NiIsImtpZCI6IlViLzRXTVplK29NMmNxdEUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3BtdWRobHFkYXphbXVncm1wa2l1LnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiIzNGY2OGUwZC1mNTlmLTQyYjgtOGEyZS0wNjY3MjlkYjEzZTgiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzM0MjE1Mjc2LCJpYXQiOjE3MzQyMTE2NzYsImVtYWlsIjoieWFubkBqb2huY2xvdWQuZnIiLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJwYXNzd29yZCIsInRpbWVzdGFtcCI6MTczNDIxMTY3Nn1dLCJzZXNzaW9uX2lkIjoiODdjZGVjOGEtMTNiOS00MGU4LWJlMDgtODJlZmE4OTUwNWM0IiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.vGv2vCuKKR1xRGn3tpff1uxokj-__sdbAv2AYUKoI3c",
-):
-    supabase: Client = create_client(config.SUPABASE_URL, config.SUPABASE_KEY)
-    response = supabase.auth.sign_in_with_password(
-        {"email": config.SUPABASE_TEST_USER, "password": config.SUPABASE_TEST_PASSWORD}
-    )
 
     response = supabase.auth.get_user(token)
     user = response.user
@@ -39,7 +28,8 @@ def test_supabase2(
     )
     print(user)
 
+    supabase.auth.sign_out()
+
 
 if __name__ == "__main__":
-    token = test_supabase()
-    test_supabase2(token)
+    test_supabase()
